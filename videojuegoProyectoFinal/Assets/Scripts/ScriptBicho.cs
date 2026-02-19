63a165f2572cfac913fd0484708a48c6ae592ac2
sergiosrs777 <sergiors130706@gmail.com>
2026-02-19 16:45:12 +0100

    update

--- videojuegoProyectoFinal/Assets/Scripts/ScriptBicho.cs
+++ videojuegoProyectoFinal/Assets/Scripts/ScriptBicho.cs
@@ -1,16 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
 public class ScriptBicho : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 8f;
+    public float fireRate = 2f; // cada cuantos segundos dispara
+    public Transform bulletsParent;
     void Start()
     {
-        
+        StartCoroutine(ShootRandom());
     }
 
-    // Update is called once per frame
-    void Update()
+    void Shoot()
     {
-        
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation, bulletsParent);
+
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.linearVelocity = Vector2.left * bulletSpeed;
     }
+    IEnumerator ShootRandom()
+    {
+        while (true)
+        {
+            float randomTime = Random.Range(0.5f, 2f); // dispara entre 0.5 y 2 segundos
+            yield return new WaitForSeconds(randomTime);
+
+            Shoot();
+        }
+    }
+
 }
